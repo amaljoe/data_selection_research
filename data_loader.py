7ea879ff53afe0b11637f3f4fac9a3a4c2b00dcb
amaljoe <amaljoe88@gmail.com>
2025-03-24 11:53:20 +0000

    implement mix-instruct data loader

---
+++ data_loader.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+from datasets import load_dataset
+from dotenv import load_dotenv
+load_dotenv()
+
+cache_dir = os.environ.get("CACHE_DIR", "./cache2")
+
+def get_mix_instruct(split, max_length):
+    ds_name = f"mix-instruct_{split}_{max_length}"
+    cache_file = os.path.join(cache_dir, 'datasets', f"{ds_name}.pkl")
+    os.makedirs(os.path.dirname(cache_file), exist_ok=True)
+    if os.path.exists(cache_file):
+        print(f'Dataset: {ds_name} found in cache, loading from cache ✅')
+        ds = pd.read_pickle(cache_file)
+    else:
+        print(f'Dataset: {ds_name} not found in cache, downloading from huggingface 🏃')
+        ds = load_dataset("llm-blender/mix-instruct")[split].to_pandas()[:max_length]
+        ds.to_pickle(cache_file)
+        print(f'Dataset: {ds_name} loaded and saved to cache ✅')
+    # instruction and input splitted by new line (space is used in paper)
+    prompts = ds['instruction'] + "\n" + ds['input']
+    references = ds['output']
+    return list(prompts), list(references)
