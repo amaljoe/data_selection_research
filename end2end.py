42f959ecb6409ee5afb0cc9b8dc80bcea230f3a3
amaljoe <amaljoe88@gmail.com>
2025-03-27 20:34:33 +0000

    Update end2end.ipynb with results

--- end2end.py
+++ end2end.py
@@ -4,4 +4,4 @@ from evaluation import compute_metrics
 
 prompts, references, ds_name = get_mix_instruct("train", 21000)
 responses, generation_name = generate_responses(prompts, 'meta-llama/Llama-3.2-3B', ds_name, 'cuda:0', batch_size=64)
-compute_metrics(responses, references, generation_name, device='cuda:0')
+print(compute_metrics(responses, references, generation_name, device='cuda:0', metrics=['bge', 'rouge']))
